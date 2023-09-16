1a911b24f47b063710d1488073559a3437671a8f
Marco199609 <marco19-96@hotmail.com>
2023-09-16 15:38:25 -0400

    Actualizando inventario

--- Assets/Scripts/Interactables/Interactable.cs
+++ Assets/Scripts/Interactables/Interactable.cs
@@ -6,11 +6,19 @@ public class Interactable : MonoBehaviour
 {
     [SerializeField] private GameObject _requiredObject;
 
+    private List<GameObject> _inventory;
+
     public void Interact()
     {
-        if(_requiredObject == null || PlayerController.Instance.Inventory.Objects[0] == _requiredObject)
+        _inventory = PlayerController.Instance.Inventory.Objects;
+
+        if (_requiredObject == null || _inventory.Count > 0 &&  _inventory[0] == _requiredObject)
         {
             GetComponent<IBehaviour>().Behaviour();
+        }
+        else
+        {
+            print("Need " + _requiredObject.name + "!");
         }
     }
 }
