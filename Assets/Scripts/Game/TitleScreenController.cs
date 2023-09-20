1482313f722b014e3e06dee2f21b5c569c45bf6b
Marco199609 <marco19-96@hotmail.com>
2023-09-20 17:37:48 -0400

    Agregado intro y final

--- Assets/Scripts/Game/TitleScreenController.cs
+++ Assets/Scripts/Game/TitleScreenController.cs
@@ -5,11 +5,58 @@ using UnityEngine.SceneManagement;
 
 public class TitleScreenController : MonoBehaviour
 {
-    [SerializeField] private AudioClip _clickClip;
+    [SerializeField] private AudioClip _splashClip;
     [SerializeField] private AudioSource _source;
+    [SerializeField] private AudioSource _menuMusicSource;
+
+    [SerializeField] private GameObject _canvas;
+    [SerializeField] private GameObject _creditsCanvas;
+    [SerializeField] private GameObject _introBackground;
+    [SerializeField] private float _moveSpeed;
+
+    private bool _loadGame;
+    [SerializeField] private float _loadLevelDelay = 4;
+
     public void LoadLevel()
     {
-        _source.PlayOneShot(_clickClip);
+        _loadGame = true;
+        _canvas.SetActive(false);
+        StartCoroutine(LevelChange(_loadLevelDelay));
+    }
+
+    private IEnumerator LevelChange(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        _loadGame = false;
+        
+        _source.PlayOneShot(_splashClip);
+        yield return new WaitForSeconds(_splashClip.length);
         SceneManager.LoadScene("Level1");
+    }
+
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+     if(_loadGame)
+        {
+            _introBackground.transform.position += Vector3.forward * Time.deltaTime * _moveSpeed;
+            _menuMusicSource.volume -= Time.deltaTime / 8;
+        }
+    }
+
+    public void ActivateCreditScreen()
+    {
+        _canvas.SetActive(false);
+        _creditsCanvas.SetActive(true);
+    }
+    public void ActivateMainScreen()
+    {
+        _creditsCanvas.SetActive(false);
+        _canvas.SetActive(true);
     }
 }
--- Assets/Scripts/Interactables/Behaviour_EndLevel.cs
+++ Assets/Scripts/Interactables/Behaviour_EndLevel.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Behaviour_EndLevel : MonoBehaviour, IBehaviour
 {
@@ -11,7 +12,13 @@ public class Behaviour_EndLevel : MonoBehaviour, IBehaviour
     [SerializeField] private float _cameraMoveSpeed;
 
     [SerializeField] private float _endLevelDelay;
-    [SerializeField] private bool _endLevel;
+    [SerializeField] private Image _endBackgroundImage;
+    [SerializeField] private GameObject _mainCanvas;
+    [SerializeField] private AudioSource _musicSource;
+    [SerializeField] private AudioClip _musicClip;
+    private bool _endLevel;
+    private bool _showEndingBackground;
+    private float _backgroundTransparency;
 
 
     public void Behaviour()
@@ -26,14 +33,25 @@ public class Behaviour_EndLevel : MonoBehaviour, IBehaviour
         {
             _cameraTransform.position = Vector3.Lerp(_cameraTransform.position, _cameraTarget.position, Time.deltaTime * _cameraMoveSpeed);
         }
+        if(_showEndingBackground)
+        {
+            _backgroundTransparency += Time.deltaTime * 0.2f;
+            _endBackgroundImage.color = new Color(1, 1, 1, _backgroundTransparency);
+        }
     }
 
     private IEnumerator EndLevel(float delay)
     {
+        _mainCanvas.SetActive(false);
         yield return new WaitForSeconds(1f);
         _endLevel = true;
-
+        _musicSource.Stop();
+        _musicSource.volume = 0.5f;
+        _musicSource.PlayOneShot(_musicClip);
         yield return new WaitForSeconds(delay);
+        _endBackgroundImage.gameObject.SetActive(true);
+        _showEndingBackground = true;
+        yield return new WaitForSeconds(7.3f);
         SceneManager.LoadScene("MainMenu");
     }
 }
